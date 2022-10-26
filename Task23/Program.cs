5c78b12e0ba13c2e139f7d61714809b6d8ada1cc
Оля <moskvinaov5@gmail.com>
2022-10-26 17:25:40 +0300

    add second version of Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -3,11 +3,30 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("Введите число ");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число ");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-for (int count = 1; count < N; count++)
+// for (int count = 1; count < N; count++)
+// {
+//      Console.Write($"{Math.Pow(count,3)}, ");
+// }
+// Console.WriteLine($"{Math.Pow(N, 3)}, ");
+
+
+void cube(int N)
 {
-     Console.Write($"{Math.Pow(count,3)}, ");
+    if (N > 0)
+    {
+        int count = 1;
+        while (count <= N)
+        {
+            Console.Write(Math.Pow(count, 3) + " ");
+            count++;
+        }
+    }
+    else Console.WriteLine($" число {N} не соответствует условию ввода. Введите число больше 1. ");
 }
-Console.WriteLine($"{Math.Pow(N, 3)}, ");
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+cube(N);
